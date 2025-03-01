55db3591d07b49ce15a53c2ee1fbb81ff187a08a
Nico Bodkin <nbodkin@gmail.com>
2025-03-01 14:46:39 -0600

    add tooltips

--- src/app.py
+++ src/app.py
@@ -25,7 +25,7 @@ cds_data['cds_exons'] = cds_data['cds_exons'].apply(eval)
 
 example_clinvar_path = str(app_dir / "example_input/clinvar_result.txt")
 example_gnomad_path = str(app_dir / "example_input/gnomAD_v4.1.0_ENSG00000103197_2024_11_03_20_28_38.csv")
-
+question_circle_fill = ui.HTML('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle-fill mb-1" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z"/></svg>')
 
 # Add page title and sidebar
 app_ui = ui.page_navbar(
@@ -47,8 +47,8 @@ app_ui = ui.page_navbar(
             ui.input_selectize("gene", label="Gene Name", choices=[], width="100%"),
             ui.input_select("transcript", label="Transcript (Refseq)", choices=[], width="100%"),
             ui.input_numeric("num_designs", label="Number of epegRNA libraries to design", value=12, step=1, min=1),
-            ui.input_file("clinvar_csv", "Choose missense ClinVar csv to upload:", multiple=False),
-            ui.input_file("gnomad_csv", "Choose gnomAD csv to upload:", multiple=False),
+            ui.input_file("clinvar_csv", ui.tooltip(ui.span("Choose missense ClinVar tsv to upload  ", question_circle_fill),"This file is generated from ClinVar after searching for your gene, filtering for missense variants, and clicking \"Download\"",placement="right"), multiple=False),
+            ui.input_file("gnomad_csv", ui.tooltip(ui.span("Choose gnomAD csv to upload  ", question_circle_fill),"This file is generated from gnomAD after searching for your gene, filtering as desired, and clicking \"Export variants\"",placement="right"), multiple=False),
             ui.input_checkbox_group(  
                 "checkbox_group",  
                 ui.span("Include additional variants in editing windows:", style="text-decoration: underline;"),  
